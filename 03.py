e7344286bb7eb307b56f8dcad065e023ca0a91ec
final-project <daliashaat2002@gmail.com>
2023-08-05 10:50:01 +0300

    task5

---
+++ 03.py
@@ -0,0 +1,16 @@
+def add(n1,n2):
+    return n1+n2
+def multipulation(n1,n2):
+    return n1*n2
+def subtraction (n1,n2):
+    return n1-n2
+def division (n1,n2):
+    return n1/n2
+
+n1 = int(input("Enter number 1:"))
+n2 = int(input("Enter number 2:"))
+
+print(add(n1,n2))
+print(multipulation(n1,n2))
+print(subtraction (n1,n2))
+print(division (n1,n2))
